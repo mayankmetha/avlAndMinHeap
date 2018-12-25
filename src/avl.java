ffd1321cacc4103cfa074d8395a9a3dc357f2cd4
mayankmetha <mayankmetha@gmail.com>
2018-12-25 08:43:42 +0530

    added avl nodes

--- src/avl.java
+++ src/avl.java
@@ -1,2 +1,23 @@
 public class avl {
+
+}
+
+class avlNode{
+    avlNode left, right;
+    int key;
+    int height;
+
+    avlNode() {
+        left = null;
+        right = null;
+        key = 0;
+        height = 0;
+    }
+
+    avlNode(int k) {
+        left = null;
+        right = null;
+        key = k;
+        height = 0;
+    }
 }
